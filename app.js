22635e76ef3428dbb4c4f915ed99441b34252366
SHAHJEE <154551822+syedzubairalishah@users.noreply.github.com>
2024-03-27 18:27:33 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var username
+
+var myName = "SYED ZUBAIR ALI SHAH"
+
+var message = "Hello World"
+alert(message)
+
+alert("jhone doe");
+
+alert("15 years old");
+
+alert("certified mobile application development")
+
+var food = "pizza \n pizz \n piz \n pi \n p"
+alert(food)
+
+var email = "syedzuabiralishah06@gmail.com"
+alert(email)
+
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from the Book A smarter way to learn Javascript")
+
+document.write("Yah! I can write HTML content through JS")
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
